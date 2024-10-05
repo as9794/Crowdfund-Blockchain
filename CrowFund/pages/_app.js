cc89739791e972bedc931467d24d3e43b218f0ae
as9794 <as9794@srmist.edu.in>
2024-10-05 16:10:21 +0530

    All the necessary files added

---
+++ CrowFund/pages/_app.js
@@ -0,0 +1,38 @@
+import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { UseWalletProvider } from "use-wallet";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "@fontsource/space-grotesk";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Space Grotesk",
+    body: "Space Grotesk",
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {" "}
+      <ChakraProvider theme={theme}>
+        <UseWalletProvider
+          chainId={4}
+          connectors={{
+            walletconnect: {
+              rpcUrl:
+                "https://goerli.infura.io/v3/OQtiK6N9Ga0ltj-pUj3crYGZuTyJHpUv",
+            },
+          }}
+        >
+          <NavBar />
+          <Component {...pageProps} />
+          <Footer />{" "}
+        </UseWalletProvider>
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default MyApp;
